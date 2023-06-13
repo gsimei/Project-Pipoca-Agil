6e1980e7a9e7c3a294c1ea8319d939fca1227961
RaquelFonsec <raque-leto@hotmail.com>
2023-06-13 22:01:52 +0200

    ajustar signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
   get "/email_confirmation", to: "pages#email_confirmation", as: "email_confirmation"
   get "/privacy", to: "pages#privacy", as: "privacy"
 end
